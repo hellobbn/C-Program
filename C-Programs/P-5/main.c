bc910b9da88cc8052b7748fafc09fe611f73887d
hellobbn <clfbbn@gmail.com>
2017-10-28 12:34:00 +0800

    P-5_main.c: Updated to version 1.1 1. Added version output 2. Updated output 3. minor bug fixes

--- C-Programs/P-5/main.c
+++ C-Programs/P-5/main.c
@@ -7,23 +7,31 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
-#define version 1.0
+#define version "1.1"
 void pfile(FILE *, FILE *);
 int main(int argc, char *argv[]) {
     FILE *fp;       //  Pointer to a file
-    int i = 1;
-    char *file_name;
+    char* var;
+
     if (argc == 1) {
-        printf("BBN's cat program:\n");
-        printf("\tVersion:%.2f", version);
+        printf("usage: cat [file]\n");
+        printf("quiting\n");
         return 1;
+    } else if(argc == 2 && **++argv == '-' && *((*argv)+1) == '-') {
+        var = (*argv) + 2;
+        if (strcmp(var, "version") == 0) {
+            printf("bbn's versio of cat, version ");
+            printf(version"\n");
+            return 0;
+        }
+        
     } else {
         while (--argc > 0) {
-            file_name = argv[i++];
-            if ((fp = fopen(file_name, "r")) == NULL) {
-                printf("cat: file %s not found\n", file_name);
-                printf("In function: %s", __FUNCTION__);
+            if ((fp = fopen(*++argv, "r")) == NULL) {
+                printf("cat: file %s not found\n", *argv);
+                printf("In function: %s\n", __FUNCTION__);
                 return 2;
             } else {
                 pfile(fp, stdout);
@@ -31,7 +39,6 @@ int main(int argc, char *argv[]) {
         }
     }
 }
-
 void pfile(FILE *infile, FILE *outfile) {
     int i;
     while ((i = getc(infile)) != EOF) {
