d5b650ce469cb37b42932f603a8aa66ae5ec5170
hellobbn <clfbbn@gmail.com>
2017-11-05 13:43:51 +0800

    Project: Update P-3/tree.c: A branch shouldn't be deleted when print the tree P-5/main.c: Updated to version 1.2 	Updated output and adjusted format 	fprint() avoids error message from not showing if you run the command "a.out > out" in Linux 	file should be closed after using

--- C-Programs/P-3/tree.c
+++ C-Programs/P-3/tree.c
@@ -35,7 +35,6 @@ void printTree(struct tree* in) {
         printTree(in->left);
         printf("%4d   %s\n", in->count, in->word);
         printTree(in->right);
-        in = NULL;
-        
+        return;
     }
 }
--- C-Programs/P-5/main.c
+++ C-Programs/P-5/main.c
@@ -9,32 +9,32 @@
 #include <stdio.h>
 #include <string.h>
 
-#define version "1.1"
+#define version "1.2"
 void pfile(FILE *, FILE *);
 int main(int argc, char *argv[]) {
     FILE *fp;       //  Pointer to a file
     char* var;
+    char *prog = argv[0];
 
     if (argc == 1) {
         printf("usage: cat [file]\n");
-        printf("quiting\n");
+        printf("Error input format! Quiting...\n");
         return 1;
-    } else if(argc == 2 && **++argv == '-' && *((*argv)+1) == '-') {
+    } else if (argc == 2 && **++argv == '-' && *((*argv)+1) == '-') {
         var = (*argv) + 2;
         if (strcmp(var, "version") == 0) {
             printf("bbn's versio of cat, version ");
             printf(version"\n");
             return 0;
         }
-        
     } else {
         while (--argc > 0) {
             if ((fp = fopen(*++argv, "r")) == NULL) {
-                printf("cat: file %s not found\n", *argv);
-                printf("In function: %s\n", __FUNCTION__);
+                fprintf(stderr, "%s: can't find file %s", prog, *argv);
                 return 2;
             } else {
                 pfile(fp, stdout);
+                fclose(fp);                         //  inverse of fopen();
             }
         }
     }
